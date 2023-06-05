6d88a44d6f4fc8cddb550175d43926e856cc14c8
EvaGraceSmith <smithevagrace@gmail.com>
2023-06-04 20:16:18 -0500

    updating files

--- hub.js
+++
@@ -1,9 +0,0 @@
-const eventPool = require('./eventPool');
-
-eventPool.onAny((event, payload) => {
-  console.log('EVENT:', {
-    event,
-    time: new Date().toISOString(),
-    payload,
-  });
-});
